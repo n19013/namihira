351310bc7521d44f58b181ed434331b391f144b5
n19013 <n19013@std.it-college.ac.jp>
2019-06-11 10:30:38 +0900

    0604

--- 0604/060404.sh
+++ 0604/060404.sh
@@ -1,10 +1,19 @@
 if [ -d "$1" ]; then
-for file in $(ls "$1")
-do
-    if [ -x "${1}/$file" ]; then
-        echo "$file"
-    fi
-done
+    #ディレクトリ内のファイルが実行可能ファイルかチェックして
+    #実行可能ファイルであれば表示
+    #for file in $(ls "$1")
+    for file in $(find "$1" -maxdepth 1 -type f)
+    do
+        # fileが実行可能であれば表示
+        #if [ -x "${1}/${file}" ]; && [ ! -d "${1}/${file}" ]; then
+        if [! -r "$file" ]; then
+            #echo "$file"
+            #echo 'basename "$file"'
+            echo $(basename "$file")
+            #sudo ls -l "${file}"
+        fi
+    done
 else
+    # エラーメッセージ
     echo "$1:ディレクトリではありません"
 fi
